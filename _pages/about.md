6e7516885fd5e3d55f80618606fc2cbbd8f4d333
Coder0951 <Tyger0951@gmail.com>
2018-09-29 04:58:36 -0500

    add posts

--- _pages/about.md
+++
@@ -1,8 +0,0 @@
----
-layout: single
-title: "About"
-permalink: /about/
-#header:
-  #image: "/images/BioPic.jpg"
----
-I am a data analysis with a back ground in Call Center workforce management!
---
+++ test.py
@@ -0,0 +1,6 @@
+import win32com.client
+word = win32com.client.Dispatch('Word.Application')
+doc = word.Documents.Add('c:/users/tyger/desktop/profile.html')
+doc.SaveAs('c:/users/tyger/desktop/example.doc', FileFormat=0)
+doc.Close()
+word.Quit()
