551fb784e47a868f4df35c979555a65f8c34f10c
Coder0951 <Tyger0951@gmail.com>
2018-09-30 04:06:50 -0500

    Update Resume

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import win32com.client
 word = win32com.client.Dispatch('Word.Application')
-doc = word.Documents.Add('c:/users/tyger/desktop/profile.html')
+doc = word.Documents.Add('c:/users/tyger/desktop/Indeed_resume.html')
 doc.SaveAs('c:/users/tyger/desktop/example.doc', FileFormat=0)
 doc.Close()
 word.Quit()
